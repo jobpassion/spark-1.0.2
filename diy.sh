6454811b92efc2b250a543c095a9dea691d79b05
Your Name <you@example.com>
2014-08-25 10:17:47 +0800

    a

--- diy.sh
+++ diy.sh
@@ -1,2 +1,3 @@
-wget -P $OPENSHIFT_DATA_DIR http://www.us.apache.org/dist/spark/spark-1.0.2/spark-1.0.2-bin-hadoop2.tgz
-tar zxf $OPENSHIFT_DATA_DIR/spark-1.0.2-bin-hadoop2.tgz
+cd $OPENSHIFT_DATA_DIR
+wget http://www.us.apache.org/dist/spark/spark-1.0.2/spark-1.0.2-bin-hadoop2.tgz
+tar zxf spark-1.0.2-bin-hadoop2.tgz
