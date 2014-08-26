f058a17c6f97f4becd288481c9fd18fe9030b233
jobpassion <jeffery@jefferydeiMac.local>
2014-08-27 00:35:49 +0800

    a

--- autossh-setup.sh
+++ autossh-setup.sh
@@ -1,1 +1,1 @@
-./autossh -N -v -M 0 -L $OPENSHIFT_DIY_IP:17077 127.5.198.129:17077
+./autossh -N -v -M 0 -L $OPENSHIFT_DIY_IP:17077 127.5.198.129:17077 -i ./id_rsa 53f7f72ce0b8cd17ed000095@sparkmaster-jobpassion5.rhcloud.com
---
+++ hadoop-setup.sh
@@ -0,0 +1,5 @@
+cd $OPENSHIFT_DATA_DIR
+wget http://www.us.apache.org/dist/hadoop/common/hadoop-2.5.0/hadoop-2.5.0.tar.gz
+tar zxf hadoop-2.5.0.tar.gz
+rm -rf hadoop-2.5.0.tar.gz
+
