856e6f194e28c3718b510cc5eae8cb1050f09503
Your Name <you@example.com>
2014-08-27 15:46:21 +0800

    a

--- autossh-setup.sh
+++ autossh-setup.sh
@@ -1,1 +1,1 @@
-./autossh -f -N -v -M 0 -L $OPENSHIFT_DIY_IP:17077:127.5.198.129:17077 -i ./id_rsa 53f7f72ce0b8cd17ed000095@sparkmaster-jobpassion5.rhcloud.com -o "StrictHostKeyChecking no"
+nohup ./autossh -N -v -M 0 -L $OPENSHIFT_DIY_IP:17077:127.5.198.129:17077 -i ./id_rsa  -o StrictHostKeyChecking=no 53f7f72ce0b8cd17ed000095@sparkmaster-jobpassion5.rhcloud.com &
