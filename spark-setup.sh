2df21da9b8929e5ccadcacdc993bdbccec971f54
Your Name <you@example.com>
2014-08-27 10:59:08 +0800

    a

--- spark-setup.sh
+++ spark-setup.sh
@@ -6,6 +6,7 @@ export SPARK_MASTER_IP=$OPENSHIFT_DIY_IP
 export SPARK_MASTER_PORT=17077
 export SPARK_WORKER_WEBUI_PORT=8080
 export SPARK_WORKER_PORT=17078
+export SPARK_WORKER_IP=$OPENSHIFT_DIY_IP
 export SPARK_UI_HOST=$OPENSHIFT_DIY_IP
 rm -rf spark-1.0.2-bin-hadoop2/lib/spark-assembly-1.0.2-hadoop2.2.0.jar
 wget -P spark-1.0.2-bin-hadoop2/lib https://jobpassion2-80.terminal.com/spark-assembly-1.0.2-hadoop2.2.0.jar
